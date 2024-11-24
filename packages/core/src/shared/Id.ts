40b28d3e547a9bd0992570d5bb03261099ea580e
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2024-11-24 18:35:08 -0300

    created function validate id

--- packages/core/src/shared/Id.ts
+++ packages/core/src/shared/Id.ts
@@ -1,7 +1,11 @@
-import { v4 } from "uuid";
+import { v4, validate } from "uuid";
 
 export default class Id {
   static new(): string {
     return v4();
+  }
+
+  static valid(id: string): boolean {
+    return validate(id);
   }
 }
