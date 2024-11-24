86a16740116fc00edbc3cae43d7044dd9dd5a9a6
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2024-11-24 18:42:57 -0300

    feat: add route to fetch all events

--- apps/backend/src/events/events.controller.ts
+++ apps/backend/src/events/events.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { completeQuest, Event, EventDate, events, Id, Quest } from 'core';
 
 @Controller('events')
-export class EventsController {}
+export class EventsController {
+
+    @Get()
+    async getEvents() {
+        return events.map(this.serializeEvent);
+    }
+
+
+    private serializeEvent(event: Event) {
+        if (!event) return null;
+        return {
+            ...event,
+            date: EventDate.toString(event.date)
+        };
+    }
+
+}
